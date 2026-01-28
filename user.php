041c62eeaa39097c8483b5101a557b2bce7f6f87
Nola <nolasadikajj@gmail.com>
2026-01-28 15:16:39 +0100

    user.php
    Perdoret per menaxhimin e perdoruesve ne webaplikacion

---
+++ user.php
@@ -0,0 +1,55 @@
+<?php
+require_once "Database.php";
+
+
+class User {
+    private $conn;
+    
+
+    public function_conctruct() {
+        $db = new Database();
+        $this->conn = $db->connect();
+
+    }
+
+    public function register($name,$email,$password) {
+        $password = password_hash($password, PASSWORD_DEAFAULT);
+
+
+        $sql = "INSERT INTO users (name, email, password, role)
+                VALUES (:name, :email, :password, 'user')";
+
+
+        $stmt = $this->conn->prepare($sql);
+        return $stmt->excute([
+            ':name'=> $name,
+            ':email'=> $email,
+            ':password'=> $password,
+        ]);
+ 
+        public function login($email , $password) {
+            $sql = "SELECT * FROM users WHERE email = :email";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->excute([':email'=> $email]);
+
+            $user = $stmt-> fetch(PDO::FETCH_ASSOC);
+
+            if($user && password_verify($password , $user['password'])){
+                session_start();
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['role'] = $user['role'];
+                return true;
+            }
+            return false;
+        }
+
+        public function isAdmin(){
+            return isset($_SESSION['role']) && $_SESSION['role'] === 'admin';
+        }
+
+        public function logout(){
+            session_start();
+            session_destroy();
+        }
+    }
+}
