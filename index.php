723e765af762c5a2d57eb05a73b100db9aa3eec8
RrezartaGjocaj <rrezartagjocaj6@gmail.com>
2026-01-29 16:59:53 +0100

    index.php

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 session_start();
 ?>
+ 
+<html>
+<body>
+   <h1>Home Page</h1>
 
-<h1>Home Page</h1>
-
-<?php if (isset($_SESSION['user_id'])): ?>
-    <p>Welcome!</p>
-    <a href="logout.php">Logout</a>
-<?php else: ?>
-    <a href="login.php">Login</a>
-<?php endif; ?>
+     <?php if (isset($_SESSION['user_id'])): ?>
+         <p>Welcome!</p>
+        <a href="logout.php">Logout</a>
+        <?php else: ?>
+     <a href="login.php">Login</a>
+    <?php endif; ?>
+ </body>
+ <html>
