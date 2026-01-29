a31f8b4b4bb7cf3b184f1835afa6ce85a8460f15
RrezartaGjocaj <rrezartagjocaj6@gmail.com>
2026-01-29 14:37:27 +0100

    contact.php

---
+++ contact.php
@@ -0,0 +1,6 @@
+<form method="POST">
+    <input type="text" name="name" placeholder="Name">
+    <input type="email" name="email" placeholder="Email">
+    <textarea name="message" placeholder="Message"></textarea>
+    <button type="submit">Send</button>
+</form>
