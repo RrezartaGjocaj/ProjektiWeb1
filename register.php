812c3bc74b31ccbed401b6ae2455412970858db9
RrezartaGjocaj <rrezartagjocaj6@gmail.com>
2026-01-29 14:19:00 +0100

    register.php

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+require_once "classes/User.php";
+
+$user = new User();
+
+if ($_POST) {
+    $user->register($_POST['name'], $_POST['email'], $_POST['password']);
+    header("Location: login.php");
+}
+?>
+
+<form method="POST">
+    <input type="text" name="name" placeholder="Name" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Register</button>
+</form>
