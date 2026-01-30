ebd8364866692d80b73ed6265c1cffc051d90a12
RrezartaGjocaj <rrezartagjocaj6@gmail.com>
2026-01-30 12:02:04 +0100

    register.php

--- register.php
+++ register.php
@@ -13,8 +13,11 @@ if ($_POST) {
 
   <form method="POST">
     <input type="text" name="name" placeholder="Name" required>
+     <input type="text" name="surname" placeholder="Surname" required>
     <input type="email" name="email" placeholder="Email" required>
     <input type="password" name="password" placeholder="Password" required>
+    <input type="password" name="password" placeholder="Confirm password" required>
+    <input type="telephone" placeholder="Telephone number" required>
     <button type="submit">Register</button>
  </form>
 
