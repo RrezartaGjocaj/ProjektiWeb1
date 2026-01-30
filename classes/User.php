11080d5bb3ac64ec1d9716705bdd1e3fc29a56c4
RrezartaGjocaj <rrezartagjocaj6@gmail.com>
2026-01-30 12:44:18 +0100

    User.php

--- classes/User.php
+++ classes/User.php
@@ -11,30 +11,41 @@ class User {
     $this->conn = $db->connect();
 }
     //CREATE
+   public function register($name, $email, $password) {
+    $hashed = password_hash($password, PASSWORD_DEFAULT);
 
-    public function register($name, $email, $password) {
-        $sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
-        $stmt = $this->conn->prepare($sql);
-        return $stmt->execute([$name, $email, $password]);
-    }
+    $sql = "INSERT INTO users (name, email, password)
+            VALUES (?, ?, ?)";
+    $stmt = $this->conn->prepare($sql);
+    return $stmt->execute([$name, $email, $hashed]);
+}
+
 
 
  
-        public function login($email , $password) {
-            $sql = "SELECT * FROM users WHERE email = :email";
-            $stmt = $this->conn->prepare($sql);
-            $stmt->execute([':email'=> $email]);
+    public function login($email, $password) {
+    $sql = "SELECT * FROM users WHERE email = :email";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute(['email' => $email]);
 
-            $user = $stmt-> fetch(PDO::FETCH_ASSOC);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            if($user && password_verify($password , $user['password'])){
-                session_start();
-                $_SESSION['user_id'] = $user['id'];
-                $_SESSION['role'] = $user['role'];
-                return true;
-            }
-            return false;
-        }
+    if ($user && password_verify($password, $user['password'])) {
+        session_start();
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['role'] = $user['role'] ?? 'user';
+        return true;
+    }
+    return false;
+}
+
+
+        //READ
+        public function getAllUsers() {
+        $sql = "SELECT * FROM users";
+        $stmt = $this->conn->query($sql);
+       return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
         public function isAdmin(){
             return isset($_SESSION['role']) && $_SESSION['role'] === 'admin';
